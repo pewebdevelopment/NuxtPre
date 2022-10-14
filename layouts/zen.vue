e12e00cdd2d317b6db967b23e175db8defdcb4d5
peiacademics <acad.paathshala@gmail.com>
2022-10-14 06:02:54 +0530

    working with chart js import statement

--- layouts/zen.vue
+++ layouts/zen.vue
@@ -37,43 +37,63 @@ const menuClick = (event, item) => {
     //
   }
 };
-
 </script>
 
 <template>
   <div>
-    <div :class="{
-      dark: styleStore.darkMode,
-      'overflow-hidden lg:overflow-visible': isAsideMobileExpanded,
-    }">
-
-
-      <div :class="[layoutAsidePadding, { 'ml-60 lg:ml-0': isAsideMobileExpanded }]" class="
-        pt-14
-        min-h-screen
-        w-screen
-        transition-position
-        lg:w-auto
-        bg-gray-50
-        dark:bg-slate-800 dark:text-slate-100
-      ">
-
-        <NavBar :menu="menuNavBar" :class="[
+    <div
+      :class="{
+        dark: styleStore.darkMode,
+        'overflow-hidden lg:overflow-visible': isAsideMobileExpanded,
+      }"
+    >
+      <div
+        :class="[
           layoutAsidePadding,
           { 'ml-60 lg:ml-0': isAsideMobileExpanded },
-        ]" @menu-click="menuClick">
-
-          <NavBarItemPlain display="flex lg:hidden" @click.prevent="isAsideMobileExpanded = !isAsideMobileExpanded">
-            <BaseIcon :path="isAsideMobileExpanded ? mdiBackburger : mdiForwardburger" size="24" />
+        ]"
+        class="
+          pt-14
+          min-h-screen
+          w-screen
+          transition-position
+          lg:w-auto
+          bg-gray-50
+          dark:bg-slate-800 dark:text-slate-100
+        "
+      >
+        <NavBar
+          :menu="menuNavBar"
+          :class="[
+            layoutAsidePadding,
+            { 'ml-60 lg:ml-0': isAsideMobileExpanded },
+          ]"
+          @menu-click="menuClick"
+        >
+          <NavBarItemPlain
+            display="flex lg:hidden"
+            @click.prevent="isAsideMobileExpanded = !isAsideMobileExpanded"
+          >
+            <BaseIcon
+              :path="isAsideMobileExpanded ? mdiBackburger : mdiForwardburger"
+              size="24"
+            />
           </NavBarItemPlain>
-          <NavBarItemPlain display="hidden lg:flex xl:hidden" @click.prevent="isAsideLgActive = true">
+          <NavBarItemPlain
+            display="hidden lg:flex xl:hidden"
+            @click.prevent="isAsideLgActive = true"
+          >
             <BaseIcon :path="mdiMenu" size="24" />
           </NavBarItemPlain>
           <NavBarItemPlain use-margin>
-            <FormControl placeholder="Search (ctrl+k)" ctrl-k-focus transparent borderless />
+            <FormControl
+              placeholder="Search (ctrl+k)"
+              ctrl-k-focus
+              transparent
+              borderless
+            />
           </NavBarItemPlain>
         </NavBar>
-
 
         <slot />
 
@@ -82,19 +102,11 @@ const menuClick = (event, item) => {
         <!-- Following was the old AsideMenu  -->
         <!-- <AsideMenu :is-aside-mobile-expanded="isAsideMobileExpanded" :is-aside-lg-active="isAsideLgActive"
           :menu="menuAside" @menu-click="menuClick" @aside-lg-close-click="isAsideLgActive = false" /> -->
-
       </div>
 
-
-     
-
       <FooterBar>
-          
-      
         <a href="#" target="_blank" class="text-blue-600"> Photon Ecademy</a>
       </FooterBar>
     </div>
-
-
   </div>
 </template>
