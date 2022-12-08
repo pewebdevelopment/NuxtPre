29d4232252128238b47a7bad20fe543259254f17
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-08 18:12:27 +0530

    ok

--- app.vue
+++ app.vue
@@ -1,34 +1,32 @@
 <script setup>
+import { useStyleStore } from "@/stores/style.js";
+import { darkModeKey, styleKey } from "@/configs/config";
+import { useMainStore } from "@/stores/main.js";
+import "@/assets/css/main.css";
 
-import { useStyleStore } from '@/stores/style.js'
-import { darkModeKey, styleKey } from '@/configs/config'
-import { useMainStore } from '@/stores/main.js'
-// import '@/assets/css/main.css'
-
-const mainStore = useMainStore()
-const styleStore = useStyleStore()
-const loading = ref(true)
-
+const mainStore = useMainStore();
+const styleStore = useStyleStore();
+const loading = ref(true);
 
 onMounted(() => {
   styleStore.setStyle(localStorage[styleKey] ?? "basic");
   /* Dark mode */
   if (
     (!localStorage[darkModeKey] &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches) ||
-    localStorage[darkModeKey] === '1'
+      window.matchMedia("(prefers-color-scheme: dark)").matches) ||
+    localStorage[darkModeKey] === "1"
   ) {
-    styleStore.setDarkMode()
+    styleStore.setDarkMode();
   }
-  loading.value = false
-})
+  loading.value = false;
+});
 useHead({
   titleTemplate: (titleChunk) => {
-    const titleBase = 'Zen Nuxt'
+    const titleBase = "Zen Nuxt";
 
-    return titleChunk ? `${titleChunk} - ${titleBase}` : titleBase
+    return titleChunk ? `${titleChunk} - ${titleBase}` : titleBase;
   },
-})
+});
 </script>
 
 <template>
@@ -38,4 +36,3 @@ useHead({
     </NuxtLayout>
   </div>
 </template>
-
